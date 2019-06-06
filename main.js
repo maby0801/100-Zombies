5af4427dc78265811ecca4c1cb3032cfa6fca48d
maby0801 <maby0801@student.miun.se>
2019-06-06 23:09:11 +0200

    100% Zombies created

---
+++ main.js
@@ -0,0 +1,111 @@
+"use strict";
+
+// ELEMENTS
+var houseWrapperEl = document.getElementById("houseContainer");
+var buttonEl = document.getElementById("buttonStart");
+var statsEl = document.getElementById("stats");
+var simulationSummary = document.getElementById("simulationSummary");
+var finalResultsEl = document.getElementById("finalResults");
+
+// VARIABLES
+var simulationCounter = 0;
+var result = [];
+
+// EVENT LISTENERS
+buttonEl.addEventListener("click", prepareSimulation, false);
+
+// FUNCTIONS
+function render(zombies, humans, houses) {
+    houseWrapperEl.innerHTML = "";
+
+    // Update progress
+    statsEl.innerHTML =
+        "<span class='iconZombie'>" + zombies.length + "</span>" +
+        "<span class='iconHuman'>" + humans + "</span>";
+
+    // Render houses and set their status
+    for (var i = 0; i < houses; i++) {
+        if (zombies.includes(i)) {
+            houseWrapperEl.innerHTML += "<div id='" + i + "' class='house zombie' />";
+        } else {
+            houseWrapperEl.innerHTML += "<div id='" + i + "' class='house human' />";
+        }
+    }
+}
+
+function prepareSimulation() {
+    // Disable inputs
+    document.getElementById("buttonStart").disabled = true;
+    document.getElementById("inputSpeed").disabled = true;
+    document.getElementById("inputHouses").disabled = true;
+    document.getElementById("inputSimulations").disabled = true;
+
+    // Set speed, houses and no. of simulations
+    var speed = document.getElementById("inputSpeed").value * 100;
+    var houses = document.getElementById("inputHouses").value;
+    var simulationLimit = document.getElementById("inputSimulations").value;
+
+    // Check user input restrictions
+    if (houses > 300) { houses = 300; }
+    if (houses < 1) { houses = 1; }
+    if (simulationLimit < 1) { simulationLimit = 1; }
+
+    buttonEl.innerHTML = "Simulation running...";
+
+    runSimulation(speed, houses, simulationLimit);
+}
+
+function runSimulation(speed, houses, simulationLimit) {
+    simulationCounter++;
+
+    if (simulationCounter > simulationLimit) {
+        endSimulation(result);
+        return;
+    }
+
+    // First zombie lives at address -1
+    var zombies = [-1];
+    var round = 0;
+    var humans = houses;
+
+    render(zombies, humans, houses);
+
+    setTimeout(function () { nextRound(); }, speed);
+
+    function nextRound() {
+        round++;
+
+        // Commit one attack per zombie
+        var horde = zombies.length;
+        for (var i = 0; i < horde; i++) {
+            var target = Math.floor(Math.random() * houses);
+
+            // Determine if attack was a hit
+            if (!zombies.includes(target)) {
+                zombies.push(target);
+                humans--;
+            }
+        }
+
+        // Update graphics
+        render(zombies, humans, houses);
+        console.log("Round: " + round + " / Zombies: " + zombies.length + " / Humans: " + humans);
+
+        if (humans > 0) {
+            setTimeout(function () { nextRound(); }, speed);
+        } else {
+            // Start next simulation
+            console.log("-------------------------")
+            simulationSummary.innerHTML += "<p>Simulation " + simulationCounter + ": <span class='rounds'>" + round + " rounds</span></p>";
+            result.push(round);
+
+            setTimeout(function () { runSimulation(speed, houses, simulationLimit) }, 800);
+        }
+    }
+}
+
+function endSimulation(result) {
+    var average = result.reduce((a, b) => a + b, 0) / result.length
+    finalResultsEl.innerHTML = "It took the zombies roughly <span class='avgRounds'>" + average + " rounds</span> to find all the houses";
+    buttonEl.innerHTML = "Simulation over<br/><a href='index.html'>Try again</a>";
+}
